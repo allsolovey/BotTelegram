7609657d0b26ccb009693b8c9fb336904c5c1d14
AlexeyS <allsolovey@gmail.com>
2020-05-05 18:22:20 +0300

    Initial Commit

---
+++ bot.py
@@ -0,0 +1,11 @@
+import telebot
+import parser
+
+#main variables
+TOKEN = ''
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start', 'go'])
+def start_handler(message):
+    bot.send_message(message.chat.id, 'Привет, когда я вырасту, я буду парсить заголовки с Хабра')
+bot.polling()
